ad0fa2b1a2ee18b09c742c5851097aaaf8ec765c
MalluBoyYT <104717297+MalluBoyYT@users.noreply.github.com>
2022-05-04 11:16:32 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,14 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  Beast
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [@Crude_X](
+https://t.me/MalluHubTG)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Join : [https://t.me/MalluHubTG)
 """
